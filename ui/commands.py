ebb125ffe0a8f454087711345a1b040eba081374
suu-b <thakur0805@gmail.com>
2025-08-05 11:53:44 +0530

    cli integration with the auth utils

--- ui/commands.py
+++ ui/commands.py
@@ -1,4 +1,10 @@
+import sys
 import typer
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from auth.auth import register_user, login_user
 
 app = typer.Typer()
 
@@ -6,8 +12,12 @@ app = typer.Typer()
 def hello(name: str):
     print(f"Hello {name}!")
 
+@app.command()
+def register(username: str, password: str):
+    print("Registration began...")
+    register_user(username, password)
 
 @app.command()
-def add(x: int, y: int):
-    print("Adding...")
-    print(f"{x} + {y} = {x + y}")
+def login(username: str, password: str):
+    print("Trying to log in...")
+    login_user(username, password)
